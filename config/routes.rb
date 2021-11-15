4681a8465f642a752cf0d47a268c0a5d594d4102
Cristina Barbieri <daeima87@gmail.com>
2021-11-15 16:51:27 +0100

    Created routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :venues do
+    resources :bookings, only: %i[new create]
+    resources :reviews, only: %i[create]
+  end
 end
