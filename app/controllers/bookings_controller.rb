71937dff3c99297e776f4b3bfc8a0911d6f36173
Robert Kutt <robert.kutt@gmail.com>
2021-11-17 12:00:42 +0100

    Booking Page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,21 @@
 class BookingsController < ApplicationController
   def new
+    @venue = Venue.find(params[:venue_id])
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @venue = Venue.find(params[:venue_id])
+    @booking.venue = @venue
+    @booking.save
+    redirect_to venues_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :total_price)
   end
 end
